4687624b1e83945b732c1d8698f7b415be672a0a
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-09-15 08:35:02 -0300

    add principios solid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,23 @@
 import { Router, Request, Response } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req: Request, res: Response) => {
-  const { name, description } = req.body;
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-  if (categoryAlreadyExists) {
-    return res.status(400).json({ error: "Category Already Exists!" });
-  }
-  categoriesRepository.create({ name, description });
+  try {
+    const { name, description } = req.body;
 
-  return res.status(201).send();
+    const createCategoryService = new CreateCategoryService(
+      categoriesRepository
+    );
+    createCategoryService.execute({ name, description });
+    return res.status(201).send();
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 });
 categoriesRoutes.get("/", (req: Request, res: Response) => {
   const categories = categoriesRepository.list();
